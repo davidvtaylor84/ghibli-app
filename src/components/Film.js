65fe77c3e569219fcdb9b8529911c30911bc8b6e
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-09-10 13:38:19 +0100

    some css

--- src/components/Film.js
+++ src/components/Film.js
@@ -6,13 +6,13 @@ const Film = ({selectedFilm})=>{
 
     return (
         <div>
-            <h3>{selectedFilm.title}{selectedFilm.original_title}</h3>
+            <h3>{selectedFilm.title}...{selectedFilm.original_title}</h3>
             <p>Release Date: {selectedFilm.release_date}</p>
             <p>Description: {selectedFilm.description}</p>
             <p>Director: {selectedFilm.director}</p>
             <p>Producer: {selectedFilm.producer}</p>
             <p>Running Time: {selectedFilm.running_time}mins</p>
-            <img src={selectedFilm.movie_banner}></img>
+            <img src={selectedFilm.image}></img>
         </div>
     )
 
